2abc9eb1bba89eb7413c4806971b0215a32ad8bf
shonansurvivors <shonansurvivors@gmail.com>
2020-01-23 22:22:27 +0900

    リレーションの追加

--- app/Article.php
+++ app/Article.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
-    //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo('App\User');
+    }
 }
