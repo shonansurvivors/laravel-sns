0a19b32f6ccb006b1a2d9bf55bebebb2c20e27cf
shonansurvivors <shonansurvivors@gmail.com>
2020-01-23 23:00:28 +0900

    コントローラーの編集

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,45 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        // ダミーデータ
-        $articles = [
-            (object)[
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーザー名1',
-                ],
-            ],
-            (object)[
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 2,
-                    'name' => 'ユーザー名2',
-                ],
-            ],
-            (object)[
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 3,
-                    'name' => 'ユーザー名3',
-                ],
-            ],
-        ];
+        $articles = Article::all()->sortByDesc('created_at');
 
         return view('article.index', ['articles' => $articles]);
     }
