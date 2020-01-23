f9b4fb886abea1144b54054c74ca29ffa0d67b60
shonansurvivors <shonansurvivors@gmail.com>
2020-01-23 20:59:59 +0900

    コントローラーの編集

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,5 +6,42 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        // ダミーデータ
+        $articles = [
+            (object)[
+                'id' => 1,
+                'title' => 'タイトル1',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object)[
+                'id' => 2,
+                'title' => 'タイトル2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object)[
+                'id' => 3,
+                'title' => 'タイトル3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object)[
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+        ];
+
+        return view('article.index', ['articles' => $articles]);
+    }
 }
