e4d6d3d6956e168ab173a7c97a2ae0d539c953ba
shonansurvivors <shonansurvivors@gmail.com>
2020-02-02 00:10:03 +0900

    ルーティングの確認と編集

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 Auth::routes();
 Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
+Route::resource('/articles', 'ArticleController')->only(['show']);
