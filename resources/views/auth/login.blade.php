b2e542ceb0ee6c728d3a38838e7ce39c48ac7d13
shonansurvivors <shonansurvivors@gmail.com>
2020-02-02 22:20:53 +0900

    ログイン画面からパスワード再設定画面へ遷移可能にする

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,6 +29,10 @@
 
                 <input type="hidden" name="remember" id="remember" value="on">
 
+                <div class="text-left">
+                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
+
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
               </form>
