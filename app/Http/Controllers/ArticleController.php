c8dada3afb0392a5f4fcc588ff3a382b0b668dbf
shonansurvivors <shonansurvivors@gmail.com>
2020-02-02 18:53:57 +0900

    ポリシーをコントローラーで使用する

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Article::class, 'article');
+    }
+
     public function index()
     {
         $articles = Article::all()->sortByDesc('created_at');
