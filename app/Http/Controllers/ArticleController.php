ac54cd3e22eb6a6dd5fe4c37aacd63eb2c828f5b
shonansurvivors <shonansurvivors@gmail.com>
2020-02-09 16:24:44 +0900

    Revert "Merge pull request #6 from shonansurvivors/chapter-4"
    This reverts commit 1d0a899e34a2d198c3fc7904de07590b088f458d, reversing
    changes made to 6e0ebd935b2786ded907666ccd0993bdbfb6b0de.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -13,18 +12,5 @@ class ArticleController extends Controller
         $articles = Article::all()->sortByDesc('created_at');
 
         return view('articles.index', ['articles' => $articles]);
-    }
-
-    public function create()
-    {
-        return view('articles.create');
-    }
-
-    public function store(ArticleRequest $request, Article $article)
-    {
-        $article->fill($request->all());
-        $article->user_id = $request->user()->id;
-        $article->save();
-        return redirect()->route('articles.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
 
 Auth::routes();
-Route::get('/', 'ArticleController@index')->name('articles.index');
-Route::resource('/articles', 'ArticleController')->except(['index'])->middleware('auth');
+Route::get('/', 'ArticleController@index');
