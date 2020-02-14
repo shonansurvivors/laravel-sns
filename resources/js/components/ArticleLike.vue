0514070820aceaf8e6dcbcf2995ba569c92020db
shonansurvivors <shonansurvivors@gmail.com>
2020-02-15 00:11:45 +0900

    7-8 Vueコンポーネントからいいね・いいね解除を行う

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -6,6 +6,7 @@
     >
       <i class="fas fa-heart mr-1"
          :class="{'red-text':this.isLikedBy}"
+         @click="clickLike"
       />
     </button>
     {{ countLikes }}
@@ -23,12 +24,43 @@
         type: Number,
         default: 0,
       },
+      authorized: {
+        type: Boolean,
+        default: false,
+      },
+      endpoint: {
+        type: String,
+      },
     },
     data() {
       return {
         isLikedBy: this.initialIsLikedBy,
         countLikes: this.initialCountLikes,
       }
+    },
+    methods: {
+      clickLike() {
+        if (!this.authorized) {
+          alert('いいね機能はログイン中のみ使用できます')
+          return
+        }
+
+        this.isLikedBy
+          ? this.unlike()
+          : this.like()
+      },
+      async like() {
+        const response = await axios.put(this.endpoint)
+
+        this.isLikedBy = true
+        this.countLikes = response.data.countLikes
+      },
+      async unlike() {
+        const response = await axios.delete(this.endpoint)
+
+        this.isLikedBy = false
+        this.countLikes = response.data.countLikes
+      },
     },
   }
 </script>
