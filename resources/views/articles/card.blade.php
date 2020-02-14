381a1c21fbc9e5905b99b0747ff9b1fcfb886b5f
shonansurvivors <shonansurvivors@gmail.com>
2020-02-14 21:23:00 +0900

    記事1件分のBladeの編集

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -69,4 +69,10 @@
       {!! nl2br(e( $article->body )) !!}
     </div>
   </div>
+  <div class="card-body pt-0 pb-2 pl-3">
+    <div class="card-text">
+      <article-like>
+      </article-like>
+    </div>
+  </div>
 </div>
