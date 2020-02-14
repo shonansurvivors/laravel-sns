918585acd33c95e20c21c015c091e4bba88af7a4
shonansurvivors <shonansurvivors@gmail.com>
2020-02-14 23:54:01 +0900

    7-6 Vueコンポーネントの編集

--- resources/js/components/ArticleLike.vue
+++ resources/js/components/ArticleLike.vue
@@ -8,7 +8,7 @@
          :class="{'red-text':this.isLikedBy}"
       />
     </button>
-    10
+    {{ countLikes }}
   </div>
 </template>
 
@@ -19,10 +19,15 @@
         type: Boolean,
         default: false,
       },
+      initialCountLikes: {
+        type: Number,
+        default: 0,
+      },
     },
     data() {
       return {
         isLikedBy: this.initialIsLikedBy,
+        countLikes: this.initialCountLikes,
       }
     },
   }
