5797e04bc418bf6993b4b7e7cc0934303c5fbc59
shonansurvivors <shonansurvivors@gmail.com>
2020-02-18 23:52:10 +0900

    9 あるユーザーをフォロー中かどうか判定するメソッドを作成する

--- app/User.php
+++ app/User.php
@@ -49,4 +49,11 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
     }
+
+    public function isFollowedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->followers->where('id', $user->id)->count()
+            : false;
+    }
 }
