46db83a232423fd5cef1b32bade1054ef7f2c7f3
shonansurvivors <shonansurvivors@gmail.com>
2020-02-18 09:58:02 +0900

    9 フォローボタンのVueコンポーネントを作成する

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button
+      class="btn-sm shadow-none border border-primary p-2"
+      :class="buttonColor"
+    >
+      <i
+        class="mr-1"
+        :class="buttonIcon"
+      ></i>
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        isFollowed: true,
+      }
+    },
+    computed: {
+      buttonColor() {
+        return this.isFollowed
+          ? 'bg-primary text-white'
+          : 'bg-white'
+      },
+      buttonIcon() {
+        return this.isFollowed
+          ? 'fas fa-user-check'
+          : 'fas fa-user-plus'
+      },
+      buttonText() {
+        return this.isFollowed
+          ? 'フォロー中'
+          : 'フォロー'
+      },
+    },
+  }
+</script>
