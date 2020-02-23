aad0e272c1a49d5dd7d0764f0b77c52de4e8ee0e
shonansurvivors <shonansurvivors@gmail.com>
2020-02-23 22:47:00 +0900

    9 フォロー中・フォロワー数を算出するアクセサの作成

--- app/User.php
+++ app/User.php
@@ -61,4 +61,14 @@ class User extends Authenticatable
             ? (bool)$this->followers->where('id', $user->id)->count()
             : false;
     }
+
+    public function getCountFollowersAttribute(): int
+    {
+        return $this->followers->count();
+    }
+
+    public function getCountFollowingsAttribute(): int
+    {
+        return $this->followings->count();
+    }
 }
