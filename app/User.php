da3df27da0dd4ecd40bf13070ddda6ab3e8798c4
shonansurvivors <shonansurvivors@gmail.com>
2020-02-24 08:18:06 +0900

    9 リレーションの追加

--- app/User.php
+++ app/User.php
@@ -61,6 +61,11 @@ class User extends Authenticatable
         return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
     }
 
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article', 'likes')->withTimestamps();
+    }
+
     public function isFollowedBy(?User $user): bool
     {
         return $user
