35fb70fa703313d7ea584d910f357ae2028d447e
shonansurvivors <shonansurvivors@gmail.com>
2020-02-23 17:05:17 +0900

    9 自身をフォローできない

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -11,13 +11,15 @@
           <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
             <i class="fas fa-user-circle fa-3x"></i>
           </a>
-          <follow-button
-            class="ml-auto"
-            :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
-            :authorized='@json(Auth::check())'
-            endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
-          >
-          </follow-button>
+          @if( Auth::id() !== $user->id )
+            <follow-button
+              class="ml-auto"
+              :initial-is-followed-by='@json($user->isFollowedBy(Auth::user()))'
+              :authorized='@json(Auth::check())'
+              endpoint="{{ route('users.follow', ['name' => $user->name]) }}"
+            >
+            </follow-button>
+          @endif
         </div>
         <h2 class="h5 card-title m-0">
           <a href="{{ route('users.show', ['name' => $user->name]) }}" class="text-dark">
