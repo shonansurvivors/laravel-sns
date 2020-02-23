4722d41d30547cec06b94a0e05decf9f8ad3052e
shonansurvivors <shonansurvivors@gmail.com>
2020-02-24 08:19:05 +0900

    9 アクションメソッドの追加

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,6 +19,18 @@ class UserController extends Controller
         ]);
     }
 
+    public function likes(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $articles = $user->likes->sortByDesc('created_at');
+
+        return view('users.likes', [
+            'user' => $user,
+            'articles' => $articles,
+        ]);
+    }
+
     public function follow(Request $request, string $name)
     {
         $user = User::where('name', $name)->first();
