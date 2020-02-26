c3230684ed9993565d81ba4a3832f0c2f7c4ec13
shonansurvivors <shonansurvivors@gmail.com>
2020-02-27 07:11:17 +0900

    10 ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 Auth::routes();
 Route::prefix('login')->name('login.')->group(function () {
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+    Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
