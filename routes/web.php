b938af31e15ee2bb8ff5ef352aa03c41e26d2c47
shonansurvivors <shonansurvivors@gmail.com>
2020-02-28 00:55:11 +0900

    10 ユーザー名登録画面表示処理用のルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ Route::prefix('login')->name('login.')->group(function () {
     Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
     Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
 });
+Route::prefix('register')->name('register.')->group(function () {
+    Route::get('/{provider}', 'Auth\RegisterController@showProviderUserRegistrationForm')->name('{provider}');
+});
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
 Route::resource('/articles', 'ArticleController')->only(['show']);
