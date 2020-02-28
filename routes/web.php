b290bd30eeb85d09e5e165a46a2fd582cbaa3be6
shonansurvivors <shonansurvivors@gmail.com>
2020-02-28 10:10:16 +0900

    10 ルーティングの追加

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::prefix('login')->name('login.')->group(function () {
 });
 Route::prefix('register')->name('register.')->group(function () {
     Route::get('/{provider}', 'Auth\RegisterController@showProviderUserRegistrationForm')->name('{provider}');
+    Route::post('/{provider}', 'Auth\RegisterController@registerProviderUser')->name('{provider}');
 });
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
