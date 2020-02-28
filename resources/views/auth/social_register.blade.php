9caf4c8fc6789d8cc8347e2fde0971d23ff8ee63
shonansurvivors <shonansurvivors@gmail.com>
2020-02-28 10:11:42 +0900

    10 Bladeの編集

--- resources/views/auth/social_register.blade.php
+++ resources/views/auth/social_register.blade.php
@@ -14,11 +14,9 @@
             @include('error_card_list')
             <div class="card-text">
               <form method="POST"
-                    action="">
+                    action="{{ route('register.{provider}', ['provider' => $provider]) }}">
                 @csrf
-
                 <input type="hidden" name="token" value="{{ $token }}">
-
                 <div class="md-form">
                   <label for="name">ユーザー名</label>
                   <input class="form-control" type="text" id="name" name="name" required>
